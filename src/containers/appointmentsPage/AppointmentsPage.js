9258b8a32fe06a371d88d58d35e1964079b7c870
Leandro <leandrocitygear@icloud.com>
2024-08-21 07:23:05 -0400

    demo

---
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const AppointmentsPage = ({ appointments, contacts, addAppointment }) => {
+  const [name, setName] = useState("")
+  const [contact, setContact] = useState("")
+  const [date, setDate] = useState("")
+  const [time, setTime] = useState("")
+  /*
+  Define state variables for 
+  appointment info
+  */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addAppointment(name, contact, date, time);
+    setName("");
+    setContact("");
+    setDate("");
+    setTime("");
+    /*
+    Add contact info and clear data  
+    */
+   
+  };
+
+  return (
+    <div>
+      <section>
+        <h2>Add Appointment</h2>
+        <AppointmentForm 
+          contacts={contacts}
+          name={name}
+          setTitle={setName}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+        />
+      </section>
+      <hr />
+      <section>
+        <h2>Appointments</h2>
+        <TileList tiles={appointments}/>
+      </section>
+    </div>
+  );
+};
